bcc98671618b6ddc86f45cb6910289bcb89013db
vlad sheverda <sheverda.vlad@mail.ru>
2016-02-16 21:43:37 +0400

    Add Price and Categories

---
+++ ClinicForAnimal1.2/Controllers/ExtraController/NavController.cs
@@ -0,0 +1,24 @@
+﻿using ClinicForAnimal1._2.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicForAnimal1._2.Controllers.ExtraController
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        private VerServices db = new VerServices();
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+            IEnumerable<string> categories = db.VeterinarianServices
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}

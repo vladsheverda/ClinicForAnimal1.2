ed63448c5980e348a2061691951cc36eab5e993c
vlad sheverda <sheverda.vlad@mail.ru>
2016-03-13 11:39:14 +0300

    Яндекс.Переводчик

--- ClinicForAnimal1.2/Controllers/AccountController.cs
+++ ClinicForAnimal1.2/Controllers/AccountController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 
 namespace ClinicForAnimal1._2.Controllers
 {
+
+    //последняя версия
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
